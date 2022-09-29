bca743ad9ee62da6c882459401a287f3f80abd95
joeol <joeoliverevans@gmail.com>
2022-09-29 21:53:02 +0100

    Rejigged the code order

--- SH.py
+++ SH.py
@@ -16,8 +16,22 @@ def halfwidth(fouriertransform):
     return right - left
 
 
+def widthlist():
+    """generates list of widths of fourier transform"""
+    widths = []
+    for x in range(5, 14):
+        imagearray = plt.imread("Images/Images2/" + str(x) + "mm circ speckle image.bmp")  # imports the image
+        column = imagearray[240, :]
+        transformed_column = np.fft.fft(column)
+        transformed_column = np.concatenate((transformed_column[372:], transformed_column[1:372]))
+        abs_transformed_column = abs(transformed_column) ** 0.5
+        width = halfwidth(abs_transformed_column)
+        widths.append(width)
+    print(widths)
+
+
 def main():
-    imagearray = plt.imread("Images/5mm circ speckle image.bmp")  # imports the image
+    imagearray = plt.imread("Images/Images2/13mm circ speckle image.bmp")  # imports the image
     print(imagearray.shape)
     column = imagearray[240, :]
     print(column)
@@ -25,7 +39,8 @@ def main():
     transformed_column = np.fft.fft(column)
     print(transformed_column)
     transformed_column = np.concatenate((transformed_column[372:], transformed_column[1:372]))
-    xvalues = np.arange(0, 744)
+    #xvalues = np.arange(0, 744)
+    xvalues = np.linspace(0, 2.97, 744)
     abs_transformed_column = abs(transformed_column)
     print(halfwidth(abs_transformed_column))
     plt.plot(xvalues[1:], abs_transformed_column)
@@ -40,19 +55,6 @@ def main():
     plt.savefig('Plots/rooted.png', bbox_inches='tight')
     plt.show()
 
-
-def widthlist():
-    """generates list of widths of fourier transform"""
-    widths = []
-    for x in range(5, 14):
-        imagearray = plt.imread("Images/" + str(x) + "mm circ speckle image.bmp")  # imports the image
-        column = imagearray[240, :]
-        transformed_column = np.fft.fft(column)
-        transformed_column = np.concatenate((transformed_column[372:], transformed_column[1:372]))
-        abs_transformed_column = abs(transformed_column) ** 0.5
-        width = halfwidth(abs_transformed_column)
-        widths.append(width)
-    print(widths)
 
 main()
 widthlist()
