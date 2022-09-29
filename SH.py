92f249f590c2b4f8daa0ace2057cd2d3ea57246c
joeol <joeoliverevans@gmail.com>
2022-09-29 16:43:45 +0100

    Added halfwidth function

--- SH.py
+++ SH.py
@@ -1,7 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-imagearray = plt.imread("Images/6mm circ speckle image.bmp")
+
+def halfwidth(fouriertransform):
+    """returns the width of the peak"""
+    halfmax = max(fouriertransform) / 2
+    for x in range(0, len(fouriertransform) - 1):
+        if fouriertransform[x] >= halfmax:
+            left = x
+            break
+    for x in range(len(fouriertransform) - 1, 0, -1):
+        if fouriertransform[x] >= halfmax:
+            right = x
+            break
+    return right - left
+
+
+imagearray = plt.imread("Images/6mm circ speckle image.bmp")  # imports the image
 print(imagearray.shape)
 column = imagearray[240, :]
 print(column)
@@ -10,12 +25,16 @@ transformed_column = np.fft.fft(column)
 print(transformed_column)
 transformed_column = np.concatenate((transformed_column[372:], transformed_column[1:372]))
 xvalues = np.arange(0, 744)
-plt.plot(xvalues[1:], abs(transformed_column))
+abs_transformed_column = abs(transformed_column)
+print(halfwidth(abs_transformed_column))
+plt.plot(xvalues[1:], abs_transformed_column)
 plt.savefig('Plots/absolute.png', bbox_inches='tight')
 plt.show()
-plt.plot(xvalues[1:], transformed_column*np.conjugate(transformed_column))
+print(halfwidth(transformed_column * np.conjugate(transformed_column)))
+plt.plot(xvalues[1:], abs(transformed_column * np.conjugate(transformed_column)))
 plt.savefig('Plots/squared.png', bbox_inches='tight')
 plt.show()
-plt.plot(xvalues[1:], (transformed_column**0.5))
+print(halfwidth(abs(transformed_column ** 0.5)))
+plt.plot(xvalues[1:], abs(transformed_column ** 0.5))
 plt.savefig('Plots/rooted.png', bbox_inches='tight')
 plt.show()
