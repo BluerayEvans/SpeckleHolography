281d84ce3538eb11dd64db24e5f72b840cb13f84
joeol <joeoliverevans@gmail.com>
2022-10-04 16:32:57 +0100

    Added images 4 Changes to graphs

--- SH.py
+++ SH.py
@@ -1,5 +1,8 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
+from os import listdir
 
 
 def halfwidth(fouriertransform):
@@ -19,18 +22,18 @@ def halfwidth(fouriertransform):
 def widthlist():
     """generates list of widths of fourier transform"""
     widths = []
-    for x in range(5, 14):
-        imagearray = plt.imread("Images/Images2/" + str(x) + "mm circ speckle image.bmp")  # imports the image
+    for x in range(5, 13):
+        imagearray = plt.imread("Images/Images3/" + str(x) + "mm circ test 3.bmp")  # imports the image
         column = imagearray[240, :]
         transformed_column = np.fft.fft(column)
         transformed_column = np.concatenate((transformed_column[372:], transformed_column[1:372]))
-        abs_transformed_column = abs(transformed_column) ** 0.5
+        abs_transformed_column = np.sqrt(transformed_column * np.conjugate(transformed_column))
         width = halfwidth(abs_transformed_column)
         widths.append(width)
 
-    plt.scatter([6, 7, 8, 9, 10, 11, 12], widths[1:-1])
+    plt.scatter([5, 6, 7, 8, 9, 10, 11, 12], widths)
 
-    plt.title('Aperture circumference vs half width max of fourier transform')
+    plt.title('Aperture circumference vs half width max')
     plt.xlabel('Aperture circumference (mm)')
     plt.ylabel('Half width max (mm)')
     plt.show()
@@ -38,7 +41,7 @@ def widthlist():
 
 
 def main():
-    imagearray = plt.imread("Images/Images2/" + circumference + "mm circ speckle image.bmp")  # imports the image
+    imagearray = plt.imread("Images/Images3/" + circumference + "mm circ test 3.bmp")  # imports the image
     print(imagearray.shape)
     column = imagearray[240, :]
     print(column)
@@ -46,31 +49,18 @@ def main():
     transformed_column = np.fft.fft(column)
     print(transformed_column)
     transformed_column = np.concatenate((transformed_column[372:], transformed_column[1:372]))
-    abs_transformed_column = abs(transformed_column)
+    abs_transformed_column = (transformed_column * np.conjugate(transformed_column)) ** 0.5
     print(halfwidth(abs_transformed_column))
     plt.plot(xvalues[1:], abs_transformed_column)
-    plt.title('Absolute values from fourier transform of 1 column of the image data for circumference: ' + circumference)
+    plt.title('Absolute value fourier transform of 1 column of the image data for circumference: ' + circumference)
     plt.xlabel('Sensor width (mm)')
     plt.ylabel('Amplitude')
     plt.savefig('Plots/absolute.png', bbox_inches='tight')
     plt.show()
-    print(halfwidth(transformed_column * np.conjugate(transformed_column)))
-    plt.plot(xvalues[1:], abs(transformed_column * np.conjugate(transformed_column)))
-    plt.title('Squared values from fourier transform of 1 column of the image data for circumference: ' + circumference)
-    plt.xlabel('Sensor width (mm)')
-    plt.ylabel('Amplitude')
-    plt.savefig('Plots/squared.png', bbox_inches='tight')
-    plt.show()
-    print(halfwidth(abs(transformed_column ** 0.5)))
-    plt.plot(xvalues[1:], abs(transformed_column ** 0.5))
-    plt.title('Rooted values from fourier transform of 1 column of the image data for circumference: ' + circumference)
-    plt.xlabel('Sensor width (mm)')
-    plt.ylabel('Amplitude')
-    plt.savefig('Plots/rooted.png', bbox_inches='tight')
-    plt.show()
 
 
-circumference = '13'
+#path = os.listdir("Images\Images3")
+circumference = '6'
 xvalues = np.linspace(0, 2.97, 744)
 main()
 print(widthlist())
